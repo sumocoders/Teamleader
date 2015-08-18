820b09a39de10cb72ac6418325cfdff66ea355b1
Miguel Stevens <migzter@gmail.com>
2015-08-18 13:25:03 +0200

    Missing getWebsite() when updating or saving a company
    Added the missing getWebsite() parameter in the ToArrayForApi method.

--- Crm/Company.php
+++ Crm/Company.php
@@ -695,6 +695,9 @@ class Company
         if ($this->getTags()) {
             $return['add_tag_by_string'] = implode(',', $this->getTags());
         }
+        if ($this->getWebsite()) {
+            $return['webiste'] = $this->getWebsite();
+        }
         
         if ($this->getExtraAddresses()) {
             foreach($this->getExtraAddresses() as $type => $data ) {
